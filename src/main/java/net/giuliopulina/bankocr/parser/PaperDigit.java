2a708ff60d08b5779a98ca61c8e69c5cbb965833
Giulio Pulina <giuliopulina@users.noreply.github.com>
2024-02-08 17:03:13 +0100

    Improved visualization of digits in PaperDigit class

--- src/main/java/net/giuliopulina/bankocr/parser/PaperDigit.java
+++ src/main/java/net/giuliopulina/bankocr/parser/PaperDigit.java
@@ -7,16 +7,36 @@ import static java.util.Optional.of;
 
 public enum PaperDigit {
 
-    ZERO(" _ | ||_|", 0),
-    ONE("     |  |", 1),
-    TWO(" _  _||_ ", 2),
-    THREE(" _  _| _|", 3),
-    FOUR("   |_|  |", 4),
-    FIVE(" _ |_  _|", 5),
-    SIX(" _ |_ |_|", 6),
-    SEVEN(" _   |  |", 7),
-    EIGHT(" _ |_||_|", 8),
-    NINE(" _ |_| _|", 9);
+    ZERO(" _ "+
+                "| |"+
+                "|_|", 0),
+    ONE("   " +
+               "  |" +
+               "  |", 1),
+    TWO(" _ " +
+               " _|" +
+               "|_ ", 2),
+    THREE(" _ " +
+                 " _|" +
+                 " _|", 3),
+    FOUR("   " +
+                 "|_|" +
+                 "  |", 4),
+    FIVE(" _ " +
+                "|_ " +
+                " _|", 5),
+    SIX(" _ " +
+               "|_ " +
+               "|_|", 6),
+    SEVEN(" _ " +
+                 "  |" +
+                 "  |", 7),
+    EIGHT(" _ " +
+                 "|_|" +
+                 "|_|", 8),
+    NINE(" _ " +
+                "|_|" +
+                " _|", 9);
 
     private final String pattern;
     private final Integer digitValue;
